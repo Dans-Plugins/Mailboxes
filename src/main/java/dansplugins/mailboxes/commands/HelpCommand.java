2763d7580fce15bc7d6ecc613219f7fa755ab222
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-28 08:56:32 -0600

    Added descriptions to the commands listed in the output of the help command.

--- src/main/java/dansplugins/mailboxes/commands/HelpCommand.java
+++ src/main/java/dansplugins/mailboxes/commands/HelpCommand.java
@@ -7,13 +7,13 @@ public class HelpCommand {
 
     public boolean execute(CommandSender sender) {
         sender.sendMessage(ChatColor.AQUA + "=== Mailboxes Commands ===");
-        sender.sendMessage(ChatColor.AQUA + "/m help");
-        sender.sendMessage(ChatColor.AQUA + "/m list");
-        sender.sendMessage(ChatColor.AQUA + "/m open");
-        sender.sendMessage(ChatColor.AQUA + "/m send");
-        sender.sendMessage(ChatColor.AQUA + "/m delete");
-        sender.sendMessage(ChatColor.AQUA + "/m archive");
-        sender.sendMessage(ChatColor.AQUA + "/m config");
+        sender.sendMessage(ChatColor.AQUA + "/m help - View a list of helpful commands.");
+        sender.sendMessage(ChatColor.AQUA + "/m list - List your messages.");
+        sender.sendMessage(ChatColor.AQUA + "/m open - Open a message.");
+        sender.sendMessage(ChatColor.AQUA + "/m send - Send a message to another player.");
+        sender.sendMessage(ChatColor.AQUA + "/m delete - Delete a message.");
+        sender.sendMessage(ChatColor.AQUA + "/m archive - Archive a message.");
+        sender.sendMessage(ChatColor.AQUA + "/m config - View or set config options.");
         return true;
     }
 
