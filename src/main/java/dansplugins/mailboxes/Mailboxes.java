f9ede08a4b6303d29f41d0259259885165b52b45
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-28 00:35:21 -0600

    Changed version to v0.5

--- src/main/java/dansplugins/mailboxes/Mailboxes.java
+++ src/main/java/dansplugins/mailboxes/Mailboxes.java
@@ -16,7 +16,7 @@ public final class Mailboxes extends JavaPlugin {
         return instance;
     }
 
-    private final String version = "v0.5-alpha-1";
+    private final String version = "v0.5";
 
     @Override
     public void onEnable() {
